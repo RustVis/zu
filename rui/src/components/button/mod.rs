f93614203ff5e5c87daa796c275cc73f25b389b7
Xu Shaohua <shaohua@biofan.org>
2021-11-17 13:38:25 +0800

    Add util

--- rui/src/components/button/mod.rs
+++ rui/src/components/button/mod.rs
@@ -2,17 +2,30 @@
 // Use of this source is governed by Apache-2.0 License that can be found
 // in the LICENSE file.
 
-use yew::{html, Children, Component, Context, Html, Properties};
+use strum_macros::Display;
+use yew::{classes, html, Children, Component, Context, Html, Properties};
 
 use crate::components::config_provider::size_context::SizeType;
+use crate::util::classes_if;
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Display)]
 pub enum ButtonType {
+    #[strum(serialize = "default")]
     Default,
+
+    #[strum(serialize = "primary")]
     Primary,
+
+    #[strum(serialize = "ghost")]
     Ghost,
+
+    #[strum(serialize = "dashed")]
     Dashed,
+
+    #[strum(serialize = "link")]
     Link,
+
+    #[strum(serialize = "text")]
     Text,
 }
 
@@ -29,10 +42,22 @@ pub enum ButtonLoading {
     Delay(u32),
 }
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Display)]
+pub enum ButtonHTMLType {
+    #[strum(serialize = "button")]
+    Button,
+
+    #[strum(serialize = "submit")]
+    Submit,
+
+    #[strum(serialize = "reset")]
+    Reset,
+}
+
 #[derive(Properties, Clone, PartialEq)]
-pub struct BaseButtonProps {
-    #[prop_or_default]
-    type_: Option<ButtonType>,
+pub struct ButtonProps {
+    #[prop_or(ButtonType::Default)]
+    type_: ButtonType,
 
     //icon: Option<>,
     #[prop_or(ButtonShape::Default)]
@@ -41,23 +66,26 @@ pub struct BaseButtonProps {
     #[prop_or(SizeType::Middle)]
     size: SizeType,
 
-    #[prop_or_default]
-    loading: Option<ButtonLoading>,
+    #[prop_or(ButtonLoading::Load(false))]
+    loading: ButtonLoading,
 
-    #[prop_or_default]
-    prefix_class: Option<String>,
+    #[prop_or_else(String::new)]
+    prefix_class: String,
 
     #[prop_or_default]
     class_name: Option<String>,
 
-    #[prop_or_default]
-    ghost: Option<bool>,
+    #[prop_or(false)]
+    ghost: bool,
 
-    #[prop_or_default]
-    danger: Option<bool>,
+    #[prop_or(false)]
+    danger: bool,
 
-    #[prop_or_default]
-    block: Option<bool>,
+    #[prop_or(false)]
+    block: bool,
+
+    #[prop_or(ButtonHTMLType::Button)]
+    html_type: ButtonHTMLType,
 
     pub children: Children,
 }
@@ -71,13 +99,49 @@ impl Button {}
 
 impl Component for Button {
     type Message = Msg;
-    type Properties = BaseButtonProps;
+    type Properties = ButtonProps;
 
     fn create(_ctx: &Context<Self>) -> Self {
         Self {}
     }
 
     fn view(&self, ctx: &Context<Self>) -> Html {
+        let props = ctx.props();
+        let type_ = props.type_.to_string();
+        let html_type = props.html_type.to_string();
+        let prefix_class = &props.prefix_class;
+        // TODO(Shaohua): Read direction from global context.
+        let direction = "";
+        let block = props.block;
+        let danger = props.danger;
+        // TODO(Shaohua): Convert size_type to string
+        let size_class = "mid";
+        let shape = "";
+        let inner_loading = false;
+
+        let class_names = classes!(
+            prefix_class,
+            classes_if(vec![
+                (format!("{}-{}", prefix_class, &type_), !type_.is_empty()),
+                (
+                    format!("{}-{}", prefix_class, &shape),
+                    shape != "default" && !shape.is_empty()
+                ),
+                (
+                    format!("{}-{}", prefix_class, &size_class),
+                    !size_class.is_empty(),
+                ),
+                //(format!("{}-icon-only", prefix_class), !children && children !== 0 && !!iconType,),
+                (format!("{}-loading", prefix_class), inner_loading,),
+                (format!("{}-block", prefix_class), block,),
+                (format!("{}-dangerous", prefix_class), danger,),
+                (format!("{}-rtl", prefix_class), direction == "rtl",),
+            ]),
+            &props.class_name,
+        );
+
+        log::debug!("button class name: {:?}", class_names);
+
         let kids: Vec<_> = ctx
             .props()
             .children
@@ -86,9 +150,11 @@ impl Component for Button {
             .collect();
         log::debug!("kids: {:?}", kids);
 
-        //{ for ctx.props().children.iter() }
         html! {
-            <button>
+            <button
+                class={class_names}
+                type={html_type}
+                >
                 { for kids }
             </button>
         }
---
+++ rui/src/util.rs
@@ -0,0 +1,13 @@
+// Copyright (c) 2021 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
+// Use of this source is governed by Apache-2.0 License that can be found
+// in the LICENSE file.
+
+use yew::{classes, Classes};
+
+pub fn classes_if(list: Vec<(String, bool)>) -> Classes {
+    classes!(list
+        .into_iter()
+        .filter(|item| item.1)
+        .map(|item| item.0)
+        .collect::<Vec<String>>())
+}
