6acde3200f9c0637d4a6734e016973ddbeace7b1
Xu Shaohua <shaohua@biofan.org>
2023-01-31 12:25:03 +0800

    icon-util: Fix lint errors

--- crates/ruicon-util/src/lib.rs
+++ crates/ruicon-util/src/lib.rs
@@ -2,17 +2,24 @@
 // Use of this source is governed by Apache-2.0 License that can be found
 // in the LICENSE file.
 
+#![deny(
+    warnings,
+    clippy::all,
+    clippy::cargo,
+    clippy::nursery,
+    clippy::pedantic
+)]
+
 pub const UPDATE_KEY: &str = "RUICON_UPDATE";
 
 /// Get inner html of an svg file, withoug <svg> root tag.
+#[must_use]
 pub fn get_svg_inner(s: &str) -> Option<&str> {
-    let start_index = match s.find("<svg") {
-        Some(index) => index,
-        None => return None,
+    let Some(start_index) = s.find("<svg") else {
+        return None
     };
-    let end_index = match s.find("</svg>") {
-        Some(index) => index,
-        None => return None,
+    let Some(end_index) = s.find("</svg>") else {
+        return None
     };
 
     let mut start_index_end = start_index;
@@ -25,15 +32,13 @@ pub fn get_svg_inner(s: &str) -> Option<&str> {
     if start_index == start_index_end || start_index_end >= end_index {
         return None;
     }
-    Some(&s[start_index_end..end_index].trim())
+    Some(s[start_index_end..end_index].trim())
 }
 
 /// Check whether RUICON module shall be refreshed.
 ///
 /// Current `RUICON_REFRESH=1` environment is used.
+#[must_use]
 pub fn need_update() -> bool {
-    match std::env::var_os(UPDATE_KEY) {
-        Some(val) => !val.is_empty() || true,
-        None => false,
-    }
+    std::env::var_os(UPDATE_KEY).map_or(false, |val| !val.is_empty())
 }
