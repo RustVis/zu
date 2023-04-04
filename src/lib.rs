735ef287bcc12dac0ec6d258072423f834a62333
Xu Shaohua <shaohua@biofan.org>
2023-04-04 15:39:59 +0800

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,12 @@
+// Copyright (c) 2023 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
+// Use of this source is governed by Apache-2.0 License that can be found
+// in the LICENSE file.
+
+#![deny(
+    warnings,
+    clippy::all,
+    clippy::cargo,
+    clippy::nursery,
+    clippy::pedantic
+)]
+#![allow(clippy::module_name_repetitions)]
