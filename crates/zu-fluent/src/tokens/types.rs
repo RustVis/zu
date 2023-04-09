10743f32ffdeb9a2dc410178751689ea2ff26797
Xu Shaohua <shaohua@biofan.org>
2023-04-09 09:03:44 +0800

    fluent: Add tokens

---
+++ crates/zu-fluent/src/tokens/types.rs
@@ -0,0 +1,33 @@
+// Copyright (c) 2023 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
+// Use of this source is governed by Apache-2.0 License that can be found
+// in the LICENSE file.
+
+type Val = &'static str;
+
+pub struct BrandVariants {
+    pub b10: Val,
+    pub b20: Val,
+    pub b30: Val,
+    pub b40: Val,
+    pub b50: Val,
+    pub b60: Val,
+    pub b70: Val,
+    pub b80: Val,
+    pub b90: Val,
+    pub b100: Val,
+    pub b110: Val,
+    pub b120: Val,
+    pub b130: Val,
+    pub b140: Val,
+    pub b150: Val,
+    pub b160: Val,
+}
+
+pub struct BorderRadiusTokens {
+    pub none: Val,
+    pub small: Val,
+    pub medium: Val,
+    pub large: Val,
+    pub xlarge: Val,
+    pub circular: Val,
+}
