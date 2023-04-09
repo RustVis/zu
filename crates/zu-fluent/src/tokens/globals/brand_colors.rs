cbe4a48e6bd54b032f5216396833db5c1b39a306
Xu Shaohua <shaohua@biofan.org>
2023-04-09 09:04:05 +0800

    fluent: Add brand colors

---
+++ crates/zu-fluent/src/tokens/globals/brand_colors.rs
@@ -0,0 +1,62 @@
+// Copyright (c) 2023 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
+// Use of this source is governed by Apache-2.0 License that can be found
+// in the LICENSE file.
+
+use crate::tokens::types::BrandVariants;
+
+pub const BRAND_WEB: BrandVariants = BrandVariants {
+    b10: "#061724",
+    b20: "#082338",
+    b30: "#0a2e4a",
+    b40: "#0c3b5e",
+    b50: "#0e4775",
+    b60: "#0f548c",
+    b70: "#115ea3",
+    b80: "#0f6cbd",
+    b90: "#2886de",
+    b100: "#479ef5",
+    b110: "#62abf5",
+    b120: "#77b7f7",
+    b130: "#96c6fa",
+    b140: "#b4d6fa",
+    b150: "#cfe4fa",
+    b160: "#ebf3fc",
+};
+
+pub const BRAND_TEAMS: BrandVariants = BrandVariants {
+    b10: "#2b2b40",
+    b20: "#2f2f4a",
+    b30: "#333357",
+    b40: "#383966",
+    b50: "#3d3e78",
+    b60: "#444791",
+    b70: "#4f52b2",
+    b80: "#5b5fc7",
+    b90: "#7579eb",
+    b100: "#7f85f5",
+    b110: "#9299f7",
+    b120: "#aab1fa",
+    b130: "#b6bcfa",
+    b140: "#c5cbfa",
+    b150: "#dce0fa",
+    b160: "#e8ebfa",
+};
+
+pub const BRAND_OFFICE: BrandVariants = BrandVariants {
+    b10: "#29130b",
+    b20: "#4d2415",
+    b30: "#792000",
+    b40: "#99482b",
+    b50: "#a52c00",
+    b60: "#c33400",
+    b70: "#e06a3f",
+    b80: "#d83b01",
+    b90: "#dd4f1b",
+    b100: "#fe7948",
+    b110: "#ff865a",
+    b120: "#ff9973",
+    b130: "#e8825d",
+    b140: "#ffb498",
+    b150: "#f4beaa",
+    b160: "#f9dcd1",
+};
--- crates/zu-fluent/src/tokens/mod.rs
+++ crates/zu-fluent/src/tokens/mod.rs
@@ -2,4 +2,7 @@
 // Use of this source is governed by Apache-2.0 License that can be found
 // in the LICENSE file.
 
-pub mod tokens;
+mod globals;
+pub use globals::*;
+
+pub mod types;
