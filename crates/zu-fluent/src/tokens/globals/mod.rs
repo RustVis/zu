2794ebad8a88137961d73c630c18ed588edaa394
Xu Shaohua <shaohua@biofan.org>
2023-04-09 09:04:13 +0800

    fluent: Add border radius

---
+++ crates/zu-fluent/src/tokens/globals/mod.rs
@@ -0,0 +1,9 @@
+// Copyright (c) 2023 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
+// Use of this source is governed by Apache-2.0 License that can be found
+// in the LICENSE file.
+
+mod border_radius;
+pub use border_radius::BORDER_RADIUS;
+
+mod brand_colors;
+pub use brand_colors::{BRAND_OFFICE, BRAND_TEAMS, BRAND_WEB};
