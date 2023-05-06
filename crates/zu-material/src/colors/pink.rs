9b7463c902bd964d5440a9679e59b2f665f7d291
Xu Shaohua <shaohua@biofan.org>
2023-05-07 00:27:55 +0800

    material: Add pink color

---
+++ crates/zu-material/src/colors/pink.rs
@@ -0,0 +1,22 @@
+// Copyright (c) 2023 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
+// Use of this source is governed by Apache-2.0 License that can be found
+// in the LICENSE file.
+
+use super::Color;
+
+pub const PINK: &Color = &Color {
+    a50: "#fce4ec",
+    a100: "#f8bbd0",
+    a200: "#f48fb1",
+    a300: "#f06292",
+    a400: "#ec407a",
+    a500: "#e91e63",
+    a600: "#d81b60",
+    a700: "#c2185b",
+    a800: "#ad1457",
+    a900: "#880e4f",
+    a1000: "#ff80ab",
+    a2000: "#ff4081",
+    a4000: "#f50057",
+    a7000: "#c51162",
+};
