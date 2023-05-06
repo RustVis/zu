9e23a4d123e8f976be26f172c0f8a4eceb13c1a1
Xu Shaohua <shaohua@biofan.org>
2023-05-06 23:35:48 +0800

    material: Add Color struct

---
+++ crates/zu-material/src/colors/mod.rs
@@ -0,0 +1,23 @@
+// Copyright (c) 2023 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
+// Use of this source is governed by Apache-2.0 License that can be found
+// in the LICENSE file.
+
+pub mod amber;
+
+#[derive(Debug, Clone)]
+pub struct Color {
+    a50: &'static str,
+    a100: &'static str,
+    a200: &'static str,
+    a300: &'static str,
+    a400: &'static str,
+    a500: &'static str,
+    a600: &'static str,
+    a700: &'static str,
+    a800: &'static str,
+    a900: &'static str,
+    a1000: &'static str,
+    a2000: &'static str,
+    a4000: &'static str,
+    a7000: &'static str,
+}
--- crates/zu-material/src/lib.rs
+++ crates/zu-material/src/lib.rs
@@ -2,3 +2,6 @@
 // Use of this source is governed by Apache-2.0 License that can be found
 // in the LICENSE file.
 
+#![allow(dead_code)]
+
+pub mod colors;
