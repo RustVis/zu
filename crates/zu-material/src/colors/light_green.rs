6197053f0b831544e5a3ee71734fb9db502cc472
Xu Shaohua <shaohua@biofan.org>
2023-05-07 00:27:12 +0800

    material: Add light green color

---
+++ crates/zu-material/src/colors/light_green.rs
@@ -0,0 +1,22 @@
+// Copyright (c) 2023 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
+// Use of this source is governed by Apache-2.0 License that can be found
+// in the LICENSE file.
+
+use super::Color;
+
+pub const LIGHT_GREEN: &Color = &Color {
+    a50: "#f1f8e9",
+    a100: "#dcedc8",
+    a200: "#c5e1a5",
+    a300: "#aed581",
+    a400: "#9ccc65",
+    a500: "#8bc34a",
+    a600: "#7cb342",
+    a700: "#689f38",
+    a800: "#558b2f",
+    a900: "#33691e",
+    a1000: "#ccff90",
+    a2000: "#b2ff59",
+    a4000: "#76ff03",
+    a7000: "#64dd17",
+};
