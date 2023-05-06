670d6c93d8af8242a126389551cbb42b58ac39e5
Xu Shaohua <shaohua@biofan.org>
2023-05-07 00:27:50 +0800

    material: Add orange color

---
+++ crates/zu-material/src/colors/orange.rs
@@ -0,0 +1,22 @@
+// Copyright (c) 2023 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
+// Use of this source is governed by Apache-2.0 License that can be found
+// in the LICENSE file.
+
+use super::Color;
+
+pub const ORANGE: &Color = &Color {
+    a50: "#fff3e0",
+    a100: "#ffe0b2",
+    a200: "#ffcc80",
+    a300: "#ffb74d",
+    a400: "#ffa726",
+    a500: "#ff9800",
+    a600: "#fb8c00",
+    a700: "#f57c00",
+    a800: "#ef6c00",
+    a900: "#e65100",
+    a1000: "#ffd180",
+    a2000: "#ffab40",
+    a4000: "#ff9100",
+    a7000: "#ff6d00",
+};
