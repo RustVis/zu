765e8e438ca52fb4cc108f41becc90385aa5a16b
Xu Shaohua <shaohua@biofan.org>
2023-05-07 00:26:49 +0800

    material: Add grey color

---
+++ crates/zu-material/src/colors/grey.rs
@@ -0,0 +1,22 @@
+// Copyright (c) 2023 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
+// Use of this source is governed by Apache-2.0 License that can be found
+// in the LICENSE file.
+
+use super::Color;
+
+pub const GREY: &Color = &Color {
+    a50: "#fafafa",
+    a100: "#f5f5f5",
+    a200: "#eeeeee",
+    a300: "#e0e0e0",
+    a400: "#bdbdbd",
+    a500: "#9e9e9e",
+    a600: "#757575",
+    a700: "#616161",
+    a800: "#424242",
+    a900: "#212121",
+    a1000: "#f5f5f5",
+    a2000: "#eeeeee",
+    a4000: "#bdbdbd",
+    a7000: "#616161",
+};
