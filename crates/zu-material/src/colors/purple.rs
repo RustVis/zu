faf5f97decd9fd3ab0cc36899e1b4a7831cf858f
Xu Shaohua <shaohua@biofan.org>
2023-05-07 00:28:01 +0800

    material: Add purple color

---
+++ crates/zu-material/src/colors/purple.rs
@@ -0,0 +1,22 @@
+// Copyright (c) 2023 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
+// Use of this source is governed by Apache-2.0 License that can be found
+// in the LICENSE file.
+
+use super::Color;
+
+pub const PURPLE: &Color = &Color {
+    a50: "#f3e5f5",
+    a100: "#e1bee7",
+    a200: "#ce93d8",
+    a300: "#ba68c8",
+    a400: "#ab47bc",
+    a500: "#9c27b0",
+    a600: "#8e24aa",
+    a700: "#7b1fa2",
+    a800: "#6a1b9a",
+    a900: "#4a148c",
+    a1000: "#ea80fc",
+    a2000: "#e040fb",
+    a4000: "#d500f9",
+    a7000: "#aa00ff",
+};
