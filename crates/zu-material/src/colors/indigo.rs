16939b0b6321b70141e992e79b663b1750225e2f
Xu Shaohua <shaohua@biofan.org>
2023-05-07 00:26:57 +0800

    material: Add indigo color

---
+++ crates/zu-material/src/colors/indigo.rs
@@ -0,0 +1,22 @@
+// Copyright (c) 2023 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
+// Use of this source is governed by Apache-2.0 License that can be found
+// in the LICENSE file.
+
+use super::Color;
+
+pub const INDIGO: &Color = &Color {
+    a50: "#e8eaf6",
+    a100: "#c5cae9",
+    a200: "#9fa8da",
+    a300: "#7986cb",
+    a400: "#5c6bc0",
+    a500: "#3f51b5",
+    a600: "#3949ab",
+    a700: "#303f9f",
+    a800: "#283593",
+    a900: "#1a237e",
+    a1000: "#8c9eff",
+    a2000: "#536dfe",
+    a4000: "#3d5afe",
+    a7000: "#304ffe",
+};
