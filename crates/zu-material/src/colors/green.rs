f827a2f800d39647df1ff344f1d4c14e5c9c7e41
Xu Shaohua <shaohua@biofan.org>
2023-05-07 00:26:41 +0800

    material: Add green color

---
+++ crates/zu-material/src/colors/green.rs
@@ -0,0 +1,22 @@
+// Copyright (c) 2023 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
+// Use of this source is governed by Apache-2.0 License that can be found
+// in the LICENSE file.
+
+use super::Color;
+
+pub const GREEN: &Color = &Color {
+    a50: "#e8f5e9",
+    a100: "#c8e6c9",
+    a200: "#a5d6a7",
+    a300: "#81c784",
+    a400: "#66bb6a",
+    a500: "#4caf50",
+    a600: "#43a047",
+    a700: "#388e3c",
+    a800: "#2e7d32",
+    a900: "#1b5e20",
+    a1000: "#b9f6ca",
+    a2000: "#69f0ae",
+    a4000: "#00e676",
+    a7000: "#00c853",
+};
