158758e23bf2728ea3a066b485d7aea0d96b5ddf
Xu Shaohua <shaohua@biofan.org>
2023-05-07 08:53:54 +0800

    Merge zu into material

--- crates/zu-material/src/lib.rs
+++ crates/zu-material/src/lib.rs
@@ -2,6 +2,14 @@
 // Use of this source is governed by Apache-2.0 License that can be found
 // in the LICENSE file.
 
+#![deny(
+    warnings,
+    clippy::all,
+    clippy::cargo,
+    clippy::nursery,
+    clippy::pedantic
+)]
 #![allow(dead_code)]
+#![allow(clippy::module_name_repetitions)]
 
 pub mod colors;
--- crates/zu/src/lib.rs
+++ crates/zu/src/lib.rs
@@ -1,17 +1,3 @@
 // Copyright (c) 2021 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
 // Use of this source is governed by Apache-2.0 License that can be found
 // in the LICENSE file.
-
-#![deny(
-    warnings,
-    clippy::all,
-    clippy::cargo,
-    clippy::nursery,
-    clippy::pedantic
-)]
-// TODO(Shaohua): Update syn to 2.0
-#![allow(clippy::module_name_repetitions, clippy::multiple_crate_versions)]
-
-pub mod components;
-pub mod util;
-pub mod config_provider;
