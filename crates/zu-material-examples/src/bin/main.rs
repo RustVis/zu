7bf332830fd41e64c24c374f5c58db0c51596c9a
Xu Shaohua <shaohua@biofan.org>
2023-05-09 16:22:49 +0800

    md: Move out of material folder

--- crates/zu-material-examples/src/bin/main.rs
+++ crates/zu-material-examples/src/bin/main.rs
@@ -2,7 +2,7 @@
 // Use of this source is governed by Apache-2.0 License that can be found
 // in the LICENSE file.
 
-use zu_material_examples_theme::run_main;
+use zu_material_examples::run_main;
 
 fn main() {
     run_main();
