e359f7415d22df87666b91eb7dc9c8ea1f80dd75
Xu Shaohua <shaohua@biofan.org>
2023-05-09 18:33:39 +0800

    material: Compile scss files with rsass

--- crates/zu-material/build.rs
+++ crates/zu-material/build.rs
@@ -2,9 +2,13 @@
 // Use of this source is governed by Apache-2.0 License that can be found
 // in the LICENSE file.
 
+use std::error::Error;
 use std::fs;
 use std::io;
 use std::io::Write;
+use std::path::Path;
+
+use rsass::{compile_scss_path, output};
 
 fn merge_themes(style_files: &[&str], output_path: &str) -> io::Result<()> {
     let mut output_file = fs::OpenOptions::new()
@@ -22,9 +26,32 @@ fn merge_themes(style_files: &[&str], output_path: &str) -> io::Result<()> {
     Ok(())
 }
 
-fn main() -> io::Result<()> {
-    merge_themes(&["src/themes/dark-palette.scss"], "themes/dark-theme.css")?;
-    merge_themes(&["src/themes/light-palette.scss"], "themes/light-theme.css")?;
+fn compile_scss<P: AsRef<Path>>(input_path: P, output_path: &str) -> Result<(), Box<dyn Error>> {
+    let format = output::Format {
+        style: output::Style::Compressed,
+        ..Default::default()
+    };
+    let css = compile_scss_path(input_path.as_ref(), format)?;
+    let mut output_file = fs::OpenOptions::new()
+        .truncate(true)
+        .create(true)
+        .write(true)
+        .open(output_path)?;
+    output_file.write_all(&css)?;
+
+    Ok(())
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    merge_themes(&["src/themes/dark-palette.scss"], "themes/dark-theme.scss")?;
+    compile_scss("themes/dark-theme.scss", "themes/dark-theme.css")?;
+
+    merge_themes(
+        &["src/themes/light-palette.scss"],
+        "themes/light-theme.scss",
+    )?;
+    compile_scss("themes/light-theme.scss", "themes/light-theme.css")?;
+
     let common_styles = [
         "src/themes/animation.scss",
         "src/themes/shape.scss",
@@ -32,5 +59,6 @@ fn main() -> io::Result<()> {
         // Components
         "src/skeleton/style.scss",
     ];
-    merge_themes(&common_styles, "themes/common-theme.css")
+    merge_themes(&common_styles, "themes/common-theme.scss")?;
+    compile_scss("themes/common-theme.scss", "themes/common-theme.css")
 }
