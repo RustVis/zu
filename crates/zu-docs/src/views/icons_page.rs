9abf8055258a3b8e9d9a100be69776b81ed4a0be
Xu Shaohua <shaohua@biofan.org>
2023-05-16 13:58:08 +0800

    docs: Add color section of icon

--- crates/zu-docs/src/views/icons_page.rs
+++ crates/zu-docs/src/views/icons_page.rs
@@ -4,15 +4,12 @@
 
 use yew::{function_component, html, Html};
 use zu::svg_icon::{Color, FontSize, SvgIcon};
+use zuicon_material::Home;
 
 use super::abc::Abc;
 
 #[function_component(IconsPage)]
 pub fn icons_page() -> Html {
-    let path = html! {
-        <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />
-    };
-
     html! {
         <div class="container">
         <h1>{"Icons"}</h1>
@@ -21,18 +18,22 @@ pub fn icons_page() -> Html {
         <p>{"If you need a custom SVG icon you can use the SvgIcon wrapper.\
             This component extends the native <svg> element:"}</p>
         <div class="demo-box">
-            <SvgIcon color={Color::Primary} font_size={FontSize::Small}>
-                {path.clone()}
+            <Home />
+
+            <SvgIcon>
+                <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />
             </SvgIcon>
-            <SvgIcon color={Color::Secondary}>
-                {path.clone()}
-            </SvgIcon>
-            <SvgIcon color={Color::Action} font_size={FontSize::Large}>
-                {path.clone()}
-            </SvgIcon>
-            <SvgIcon color={Color::Disabled} style="font-size: 50px">
-                {path.clone()}
-            </SvgIcon>
+        </div>
+
+        <h3>{"Color"}</h3>
+        <div class="demo-box">
+            <Home />
+            <Home color={Color::Primary} />
+            <Home color={Color::Secondary} />
+            <Home color={Color::Success} />
+            <Home color={Color::Action} />
+            <Home color={Color::Disabled} />
+            <Home style="color: var(--zu-colors-pink-500);" />
         </div>
 
         <Abc color={Color::Error} font_size={FontSize::Large} />
