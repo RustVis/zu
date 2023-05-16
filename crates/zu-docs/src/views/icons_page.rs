1c43abeccefe783394b310c6fa59a9cd938ce2dc
Xu Shaohua <shaohua@biofan.org>
2023-05-16 13:59:14 +0800

    docs: Merge abc icon

--- crates/zu-docs/src/views/icons_page.rs
+++ crates/zu-docs/src/views/icons_page.rs
@@ -3,10 +3,24 @@
 // in the LICENSE file.
 
 use yew::{function_component, html, Html};
-use zu::svg_icon::{Color, FontSize, SvgIcon};
+use zu::svg_icon::{Color, FontSize, Props, SvgIcon};
 use zuicon_material::Home;
 
-use super::abc::Abc;
+#[function_component(Abc)]
+pub fn abc(props: &Props) -> Html {
+    html! {
+        <SvgIcon classes={props.classes.clone()}
+            color={props.color}
+            font_size={props.font_size}
+            html_color={props.html_color.clone()}
+            style={props.style.clone()}
+            title_access={props.title_access.clone()}
+            view_box={props.view_box.clone()}
+            >
+                <path d="M21,11h-1.5v-0.5h-2v3h2V13H21v1c0,0.55-0.45,1-1,1h-3c-0.55,0-1-0.45-1-1v-4c0-0.55,0.45-1,1-1h3c0.55,0,1,0.45,1,1V11z M8,10v5H6.5v-1.5h-2V15H3v-5c0-0.55,0.45-1,1-1h3C7.55,9,8,9.45,8,10z M6.5,10.5h-2V12h2V10.5z M13.5,12c0.55,0,1,0.45,1,1v1 c0,0.55-0.45,1-1,1h-4V9h4c0.55,0,1,0.45,1,1v1C14.5,11.55,14.05,12,13.5,12z M11,10.5v0.75h2V10.5H11z M13,12.75h-2v0.75h2V12.75z"/>
+        </SvgIcon>
+    }
+}
 
 #[function_component(IconsPage)]
 pub fn icons_page() -> Html {
--- crates/zu-docs/src/views/mod.rs
+++ crates/zu-docs/src/views/mod.rs
@@ -2,7 +2,6 @@
 // Use of this source is governed by Apache-2.0 License that can be found
 // in the LICENSE file.
 
-mod abc;
 pub mod badge_page;
 pub mod box_page;
 pub mod container_page;
