b8ccf7af73ca50841336bd9b29885a52182c09f4
Xu Shaohua <shaohua@biofan.org>
2023-05-16 13:41:40 +0800

    material-icon: Add custom mod

--- crates/zuicon-material/src/custom.rs
+++ crates/zuicon-material/src/custom.rs
@@ -3,3 +3,63 @@
 // in the LICENSE file.
 
 
+#[cfg(feature = "Apple")]
+mod apple;
+#[cfg(feature = "Apple")]
+pub use apple::Apple;
+
+#[cfg(feature = "Facebook")]
+mod facebook;
+#[cfg(feature = "Facebook")]
+pub use facebook::Facebook;
+
+#[cfg(feature = "GitHub")]
+mod git_hub;
+#[cfg(feature = "GitHub")]
+pub use git_hub::GitHub;
+
+#[cfg(feature = "Google")]
+mod google;
+#[cfg(feature = "Google")]
+pub use google::Google;
+
+#[cfg(feature = "Instgram")]
+mod instgram;
+#[cfg(feature = "Instgram")]
+pub use instgram::Instgram;
+
+#[cfg(feature = "LinkedIn")]
+mod linked_in;
+#[cfg(feature = "LinkedIn")]
+pub use linked_in::LinkedIn;
+
+#[cfg(feature = "Pinterest")]
+mod pinterest;
+#[cfg(feature = "Pinterest")]
+pub use pinterest::Pinterest;
+
+#[cfg(feature = "Reddit")]
+mod reddit;
+#[cfg(feature = "Reddit")]
+pub use reddit::Reddit;
+
+#[cfg(feature = "Telegram")]
+mod telegram;
+#[cfg(feature = "Telegram")]
+pub use telegram::Telegram;
+
+#[cfg(feature = "Twitter")]
+mod twitter;
+#[cfg(feature = "Twitter")]
+pub use twitter::Twitter;
+
+#[cfg(feature = "WhatsApp")]
+mod whats_app;
+#[cfg(feature = "WhatsApp")]
+pub use whats_app::WhatsApp;
+
+#[cfg(feature = "YouTube")]
+mod you_tube;
+#[cfg(feature = "YouTube")]
+pub use you_tube::YouTube;
+
---
+++ crates/zuicon-material/src/custom/you_tube.rs
@@ -0,0 +1,25 @@
+// Copyright (c) 2023 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
+// Use of this source is governed by Apache-2.0 License that can be found
+// in the LICENSE file.
+
+// Auto generated, do not edit.
+
+use yew::{function_component, html, Html};
+use zu::svg_icon::{Props, SvgIcon};
+
+#[function_component(YouTube)]
+pub fn abc(props: &Props) -> Html {
+    html! {
+        <SvgIcon classes={props.classes.clone()}
+            color={props.color}
+            font_size={props.font_size}
+            html_color={props.html_color.clone()}
+            style={props.style.clone()}
+            title_access={props.title_access.clone()}
+            icon="YouTube"
+            view_box={props.view_box.clone()}
+            >
+            <path d="M10 15l5.19-3L10 9v6m11.56-7.83c.13.47.22 1.1.28 1.9.07.8.1 1.49.1 2.09L22 12c0 2.19-.16 3.8-.44 4.83-.25.9-.83 1.48-1.73 1.73-.47.13-1.33.22-2.65.28-1.3.07-2.49.1-3.59.1L12 19c-4.19 0-6.8-.16-7.83-.44-.9-.25-1.48-.83-1.73-1.73-.13-.47-.22-1.1-.28-1.9-.07-.8-.1-1.49-.1-2.09L2 12c0-2.19.16-3.8.44-4.83.25-.9.83-1.48 1.73-1.73.47-.13 1.33-.22 2.65-.28 1.3-.07 2.49-.1 3.59-.1L12 5c4.19 0 6.8.16 7.83.44.9.25 1.48.83 1.73 1.73z"/>
+        </SvgIcon>
+    }
+}
