a970a5f0780cb735869f4fb413594d49c28ecacf
Xu Shaohua <shaohua@biofan.org>
2023-05-19 10:11:03 +0800

    zu-util: Add comments in lib

--- crates/zu-util/src/lib.rs
+++ crates/zu-util/src/lib.rs
@@ -2,6 +2,28 @@
 // Use of this source is governed by Apache-2.0 License that can be found
 // in the LICENSE file.
 
+//! # Zu util
+//!
+//! ![Build status](https://github.com/RustVis/zu/actions/workflows/rust.yml/badge.svg)
+//! [![Latest version](https://img.shields.io/crates/v/zu-util.svg)](https://crates.io/crates/zu-util)
+//! [![Documentation](https://docs.rs/zu-util/badge.svg)](https://docs.rs/zu-util)
+//! ![Minimum rustc version](https://img.shields.io/badge/rustc-1.56+-yellow.svg)
+//! ![License](https://img.shields.io/crates/l/zu-util.svg)
+//!
+//! Shared utilities for [zu](https://github.com/RustVis/zu) web component.
+//!
+//! - [Documentation](https://docs.rs/zu-util)
+//! - [Release notes](https://github.com/RustVis/zu-util/releases)
+//!
+//! ## Usage
+//! Add this to `Cargo.toml`:
+//! ```toml
+//! [dependencies]
+//! zu-util = "0.1"
+//! ```
+//!
+//! Or using `cargo add zu-util` command.
+
 #![deny(
     warnings,
     clippy::all,
