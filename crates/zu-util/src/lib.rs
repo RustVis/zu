a02a58398ffb0a042b5c5b60afab6927e8ff5399
Xu Shaohua <shaohua@biofan.org>
2023-05-19 10:04:30 +0800

    zu-util: Enable clippy lint

--- crates/zu-util/src/lib.rs
+++ crates/zu-util/src/lib.rs
@@ -2,4 +2,12 @@
 // Use of this source is governed by Apache-2.0 License that can be found
 // in the LICENSE file.
 
+#![deny(
+    warnings,
+    clippy::all,
+    clippy::cargo,
+    clippy::nursery,
+    clippy::pedantic
+)]
+
 pub mod name;
