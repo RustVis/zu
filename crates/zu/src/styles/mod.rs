a2d1fd991aaf22245105fb037359a7d6ae9193ff
Xu Shaohua <shaohua@biofan.org>
2023-05-23 08:07:01 +0800

    zu: Add severity level

--- crates/zu/src/styles/mod.rs
+++ crates/zu/src/styles/mod.rs
@@ -5,6 +5,7 @@
 pub mod color;
 pub mod direction;
 pub mod orientation;
+pub mod severity;
 pub mod shape_variant;
 pub mod side;
 pub mod size;
---
+++ crates/zu/src/styles/severity.rs
@@ -0,0 +1,34 @@
+// Copyright (c) 2023 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
+// Use of this source is governed by Apache-2.0 License that can be found
+// in the LICENSE file.
+
+use yew::AttrValue;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum Severity {
+    Success,
+    Info,
+    Warning,
+    Error,
+}
+
+impl Default for Severity {
+    fn default() -> Self {
+        Self::Success
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum SeverityColor {
+    Success,
+    Info,
+    Warning,
+    Error,
+    Custom(AttrValue),
+}
+
+impl Default for SeverityColor {
+    fn default() -> Self {
+        Self::Success
+    }
+}
