5901dd29b3155f3345e6bb451c417635f7869231
Xu Shaohua <shaohua@biofan.org>
2023-05-25 15:57:24 +0800

    zu: Add ListItem

--- crates/zu/src/list_item/mod.rs
+++ crates/zu/src/list_item/mod.rs
@@ -4,5 +4,52 @@
 
 mod align_items;
 
+use yew::{function_component, html, AttrValue, Children, Html, Properties};
 // Re-export
 pub use align_items::AlignItems;
+
+#[derive(Debug, Clone, PartialEq, Properties)]
+pub struct Props {
+    #[prop_or_default]
+    pub align_items: AlignItems,
+
+    #[prop_or_default]
+    pub children: Children,
+
+    #[prop_or_default]
+    pub classes: AttrValue,
+
+    #[prop_or_default]
+    pub component: AttrValue,
+    // TODO(Shaohua): Add container component
+    /// If true, compact vertical padding designed for keyboard and mouse input is used.
+    #[prop_or(false)]
+    pub dense: bool,
+
+    /// If true, the left and right padding is removed.
+    #[prop_or(false)]
+    pub disable_gutters: bool,
+
+    /// If true, all padding is removed.
+    #[prop_or(false)]
+    pub disable_padding: bool,
+
+    /// If true, a 1px light border is added to the bottom of the list item.
+    #[prop_or(false)]
+    pub divider: bool,
+
+    /// If true, a 1px light border is added to the bottom of the list item.
+    #[prop_or_default]
+    pub secondary_action: Option<Html>,
+
+    #[prop_or_default]
+    pub style: AttrValue,
+}
+
+#[function_component(ListItem)]
+pub fn list_item(_props: &Props) -> Html {
+    html! {
+        <>
+        </>
+    }
+}
