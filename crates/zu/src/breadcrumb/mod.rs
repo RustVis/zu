2d4c2f217e5a76431e178b9f4fae2cb31ba40829
Xu Shaohua <shaohua@biofan.org>
2023-05-26 10:07:00 +0800

    zu: Add Breadcrumb

---
+++ crates/zu/src/breadcrumb/mod.rs
@@ -0,0 +1,69 @@
+// Copyright (c) 2023 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
+// Use of this source is governed by Apache-2.0 License that can be found
+// in the LICENSE file.
+
+use yew::{function_component, html, AttrValue, Children, Html, Properties};
+
+use crate::typography::Typography;
+
+pub const DEFAULT_EXPAND_TEXT: &str = "Show path";
+pub const DEFAULT_ITEMS_AFTER_COLLAPSE: i32 = 1;
+pub const DEFAULT_ITEMS_BEFORE_COLLAPSE: i32 = 1;
+pub const DEFAULT_MAX_ITEMS: i32 = 8;
+pub const DEFAULT_SEPARATOR: &str = "/";
+
+#[derive(Debug, Clone, PartialEq, Properties)]
+pub struct Props {
+    #[prop_or_default]
+    pub children: Children,
+
+    #[prop_or_default]
+    pub classes: AttrValue,
+
+    #[prop_or_default]
+    pub component: AttrValue,
+
+    /// Override the default label for the expand button.
+    #[prop_or_default]
+    pub expand_text: AttrValue,
+
+    /// If max items is exceeded, the number of items to show after the ellipsis.
+    #[prop_or(DEFAULT_ITEMS_AFTER_COLLAPSE)]
+    pub items_after_collapse: i32,
+
+    /// If max items is exceeded, the number of items to show before the ellipsis.
+    #[prop_or(DEFAULT_ITEMS_BEFORE_COLLAPSE)]
+    pub items_before_collapse: i32,
+
+    /// Specifies the maximum number of breadcrumbs to display.
+    #[prop_or(DEFAULT_MAX_ITEMS)]
+    pub max_items: i32,
+
+    /// Custom separator node.
+    ///
+    /// Default is "/".
+    #[prop_or_default]
+    pub separator: Option<Html>,
+
+    #[prop_or_default]
+    pub style: AttrValue,
+}
+
+#[function_component(Breadcrumb)]
+pub fn breadcrumb(props: &Props) -> Html {
+    let component = if props.component.is_empty() {
+        AttrValue::from("nav")
+    } else {
+        props.component.clone()
+    };
+
+    html! {
+        <Typography
+            component={component}
+            classes="ZuBreadcrumb-root"
+            style={&props.style}>
+            <ol class="ZuBreadcrumb-ol">
+            </ol>
+        </Typography>
+    }
+}
