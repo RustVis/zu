4e9bbed06a565a2743557861b8374e85f452ac89
Xu Shaohua <shaohua@biofan.org>
2023-05-26 12:30:32 +0800

    zu: Add generate svg icons in build script

---
+++ crates/zu/src/internal/svg_icons/template.rs
@@ -0,0 +1,21 @@
+// Copyright (c) 2023 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
+// Use of this source is governed by Apache-2.0 License that can be found
+// in the LICENSE file.
+
+use yew::{function_component, html, AttrValue, Html};
+
+use crate::svg_icon::{Props, SvgIcon};
+
+#[function_component(Add)]
+pub fn add(props: &Props) -> Html {
+    let new_props = Props {
+        icon: AttrValue::from("Add"),
+        ..props.clone()
+    };
+
+    html! {
+        <SvgIcon ..new_props>
+            <path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z" />
+        </SvgIcon>
+    }
+}
