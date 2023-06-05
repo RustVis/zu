00f8414880f13b7596fcff545c7eec2b28a06d1f
Xu Shaohua <shaohua@biofan.org>
2023-06-05 20:43:45 +0800

    Add zu-base

---
+++ crates/zu-base/src/click_away_listener/event_type.rs
@@ -0,0 +1,30 @@
+// Copyright (c) 2023 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
+// Use of this source is governed by Apache-2.0 License that can be found
+// in the LICENSE file.
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum MouseEventType {
+    OnClick,
+    OnMouseDown,
+    OnMouseUp,
+    OnPointerDown,
+    OnPointerUp,
+}
+
+impl Default for MouseEventType {
+    fn default() -> Self {
+        Self::OnClick
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum TouchEventType {
+    OnTouchEnd,
+    OnTouchStart,
+}
+
+impl Default for TouchEventType {
+    fn default() -> Self {
+        Self::OnTouchEnd
+    }
+}
---
+++ crates/zu-base/src/click_away_listener/mod.rs
@@ -0,0 +1,31 @@
+// Copyright (c) 2023 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
+// Use of this source is governed by Apache-2.0 License that can be found
+// in the LICENSE file.
+
+mod event_type;
+
+use yew::{function_component, html, Callback, Children, Html, Properties};
+
+pub use event_type::MouseEventType;
+pub use event_type::TouchEventType;
+
+#[derive(Debug, Clone, PartialEq, Properties)]
+pub struct Props {
+    pub children: Children,
+
+    pub on_click_away: Callback<()>,
+
+    #[prop_or(Some(MouseEventType::default()))]
+    pub mouse_event: Option<MouseEventType>,
+
+    #[prop_or(Some(TouchEventType::default()))]
+    pub touch_event: Option<TouchEventType>,
+}
+
+#[function_component(ClickAwayListener)]
+pub fn click_away_listener(_props: &Props) -> Html {
+    html! {
+        <>
+        </>
+    }
+}
