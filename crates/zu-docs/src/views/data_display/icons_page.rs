790ab9599f8664af8cb88e14839eb1f592942527
Xu Shaohua <shaohua@biofan.org>
2023-07-12 15:02:47 +0800

    docs: Add Plus icon in Icon page

--- crates/zu-docs/src/views/data_display/icons_page.rs
+++ crates/zu-docs/src/views/data_display/icons_page.rs
@@ -2,26 +2,26 @@
 // Use of this source is governed by Apache-2.0 License that can be found
 // in the LICENSE file.
 
-use yew::{function_component, html, Html};
+use yew::{function_component, html, AttrValue, Html};
 use zu::styles::color::DisabledColor;
 use zu::svg_icon::{FontSize, Props, SvgIcon};
 use zuicon_material::Home;
 
 use crate::components::demo_box::DemoBox;
 
-#[function_component(Abc)]
-pub fn abc(props: &Props) -> Html {
-    // TODO(Shaohua): Replace with icon util.
+#[function_component(Plus)]
+pub fn plus(props: &Props) -> Html {
+    let new_props = Props {
+        icon: From::from("Plus"),
+        stroke: AttrValue::from("currentColor"),
+        fill: AttrValue::from("none"),
+        stroke_width: Some(1.5),
+        ..props.clone()
+    };
+
     html! {
-        <SvgIcon classes={props.classes.clone()}
-            color={props.color}
-            font_size={props.font_size}
-            html_color={props.html_color.clone()}
-            style={props.style.clone()}
-            title_access={props.title_access.clone()}
-            view_box={props.view_box.clone()}
-            >
-                <path d="M21,11h-1.5v-0.5h-2v3h2V13H21v1c0,0.55-0.45,1-1,1h-3c-0.55,0-1-0.45-1-1v-4c0-0.55,0.45-1,1-1h3c0.55,0,1,0.45,1,1V11z M8,10v5H6.5v-1.5h-2V15H3v-5c0-0.55,0.45-1,1-1h3C7.55,9,8,9.45,8,10z M6.5,10.5h-2V12h2V10.5z M13.5,12c0.55,0,1,0.45,1,1v1 c0,0.55-0.45,1-1,1h-4V9h4c0.55,0,1,0.45,1,1v1C14.5,11.55,14.05,12,13.5,12z M11,10.5v0.75h2V10.5H11z M13,12.75h-2v0.75h2V12.75z"/>
+        <SvgIcon ..new_props>
+            <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
         </SvgIcon>
     }
 }
@@ -54,7 +54,24 @@ pub fn icons_page() -> Html {
             <Home style="color: var(--zu-colors-pink-500);" />
         </DemoBox>
 
-        <Abc color={DisabledColor::Error} font_size={FontSize::Large} />
+        <h3>{"Size"}</h3>
+        <DemoBox>
+            <Home font_size={FontSize::Small} />
+            <Home />
+            <Home font_size={FontSize::Large} />
+            <Home style={"font-size: 40px"} />
+        </DemoBox>
+
+        <h3>{"createSvgIcon"}</h3>
+        <p>{"The createSvgIcon utility component is used to create the Material Icons. \
+        It can be used to wrap an <svg> element or an SVG path \
+        which is passed as a child to the SvgIcon component."}</p>
+        <DemoBox>
+            <Home />
+            <Home color={DisabledColor::Primary} />
+            <Plus />
+            <Plus color={DisabledColor::Secondary} />
+        </DemoBox>
 
         </div>
     }
