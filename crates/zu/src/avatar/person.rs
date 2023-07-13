5bd9d09d4b1b2450c79847088b72d4f4bcfc665f
Xu Shaohua <shaohua@biofan.org>
2023-07-13 23:23:43 +0800

    zu: Simplify Person icon in Avatar

--- crates/zu/src/avatar/person.rs
+++ crates/zu/src/avatar/person.rs
@@ -2,20 +2,19 @@
 // Use of this source is governed by Apache-2.0 License that can be found
 // in the LICENSE file.
 
-use yew::{function_component, html, AttrValue, Html, Properties};
+use yew::{function_component, html, AttrValue, Html};
 
-use crate::svg_icon::SvgIcon;
-
-#[derive(Debug, Clone, PartialEq, Eq, Properties)]
-pub struct Props {
-    #[prop_or_default]
-    pub classes: AttrValue,
-}
+use crate::svg_icon::{Props, SvgIcon};
 
 #[function_component(Person)]
 pub fn person(props: &Props) -> Html {
+    let new_props = Props {
+        icon: AttrValue::from("Person"),
+        ..props.clone()
+    };
+
     html! {
-       <SvgIcon classes={&props.classes}>
+       <SvgIcon ..new_props>
            <path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z" />
        </SvgIcon>
     }
