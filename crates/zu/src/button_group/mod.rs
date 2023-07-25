74d2621fd5601f2c226c89f7accd333cc3aef778
Xu Shaohua <shaohua@biofan.org>
2023-07-25 09:02:35 +0800

    zu: Pass props from ButtonGroup to Buttons

--- crates/zu/src/button_group/mod.rs
+++ crates/zu/src/button_group/mod.rs
@@ -2,13 +2,19 @@
 // Use of this source is governed by Apache-2.0 License that can be found
 // in the LICENSE file.
 
-use yew::{function_component, html, AttrValue, Children, Classes, Html, Properties};
+use std::rc::Rc;
+use yew::{
+    classes, function_component, html, AttrValue, ChildrenWithProps, Classes, Html, Properties,
+};
 use zu_util::prop::ToAttr;
 
+use crate::button::{Button, Props as ButtonProps};
 use crate::styles::button_variant::ButtonVariant;
 use crate::styles::color::Color;
 use crate::styles::orientation::Orientation;
 use crate::styles::size::Size;
+
+const BUTTON_CLS: &str = "ZuButtonGroup-button";
 
 #[derive(Debug, Clone, PartialEq, Properties)]
 pub struct Props {
@@ -16,15 +22,15 @@ pub struct Props {
     pub aria_label: AttrValue,
 
     #[prop_or_default]
-    pub children: Children,
+    pub children: ChildrenWithProps<Button>,
 
     #[prop_or_default]
     pub classes: Classes,
 
-    #[prop_or_default]
+    #[prop_or(Color::Primary)]
     pub color: Color,
 
-    #[prop_or_default]
+    #[prop_or(AttrValue::from("div"))]
     pub component: AttrValue,
 
     /// If true, the component is disabled.
@@ -48,26 +54,51 @@ pub struct Props {
     pub full_width: bool,
 
     /// The component orientation (layout flow direction).
-    #[prop_or_default]
+    #[prop_or(Orientation::Horizontal)]
     pub orientation: Orientation,
 
     /// The size of the component. small is equivalent to the dense button styling.
-    #[prop_or_default]
+    #[prop_or(Size::Medium)]
     pub size: Size,
 
     #[prop_or_default]
     pub style: AttrValue,
 
     /// The variant to use.
-    #[prop_or_default]
+    #[prop_or(ButtonVariant::Outlined)]
     pub variant: ButtonVariant,
 }
 
 #[function_component(ButtonGroup)]
 pub fn button_group(props: &Props) -> Html {
+    let root_cls = classes!("ZuButtonGroup-root", props.classes.clone(),);
+
+    let children: Vec<_> = props
+        .children
+        .iter()
+        .map(|mut item| {
+            let p = Rc::<ButtonProps>::make_mut(&mut item.props);
+            p.classes.push(BUTTON_CLS);
+            p.color = props.color;
+            p.disabled = props.disabled;
+            p.disable_elevation = props.disable_elevation;
+            p.disable_focus_ripple = props.disable_focus_ripple;
+            p.disable_ripple = props.disable_ripple;
+            p.full_width = props.full_width;
+            p.size = props.size;
+            p.variant = props.variant;
+            item
+        })
+        .collect();
+    let children = ChildrenWithProps::new(children);
+
     html! {
-        <div
-            aria_label={props.aria_label.to_attr()}>
-        </div>
+        <@{props.component.to_string()}
+            aria_label={props.aria_label.to_attr()}
+            class={root_cls}
+            style={props.style.clone()}
+            >
+            {for children.iter()}
+        </@>
     }
 }
