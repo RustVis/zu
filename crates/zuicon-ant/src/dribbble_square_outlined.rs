6b1e25ef2f8dfca656a3c134ba6a06f200e70124
Xu Shaohua <shaohua@biofan.org>
2023-09-22 10:03:29 +0800

    zuicon-ant: Update icons from upstream

--- crates/zuicon-ant/src/dribbble_square_outlined.rs
+++ crates/zuicon-ant/src/dribbble_square_outlined.rs
@@ -45,12 +45,13 @@ impl Component for DribbbleSquareOutlined {
         }
         html! {
             <svg
+                xmlns={ "http://www.w3.org/2000/svg" }
                 class={ props.class.unwrap_or("") }
-                width={ props.width.unwrap_or("1em") }
-                height={ props.height.unwrap_or("1em") }
+                width={ props.width.unwrap_or("16") }
+                height={ props.height.unwrap_or("16") }
                 focusable={ "false" }
                 data-icon={ "dribbble-square" }
-                viewBox={ "0 0 1024 1024" }
+                viewBox={ "0 0 16 16" }
                 fill={ props.fill.unwrap_or("currentColor") }
                 style={ style }
             >
--- crates/zuicon-ant/src/plus_outlined.rs
+++ crates/zuicon-ant/src/plus_outlined.rs
@@ -45,16 +45,18 @@ impl Component for PlusOutlined {
         }
         html! {
             <svg
+                xmlns={ "http://www.w3.org/2000/svg" }
                 class={ props.class.unwrap_or("") }
-                width={ props.width.unwrap_or("1em") }
-                height={ props.height.unwrap_or("1em") }
+                width={ props.width.unwrap_or("16") }
+                height={ props.height.unwrap_or("16") }
                 focusable={ "false" }
                 data-icon={ "plus" }
-                viewBox={ "0 0 1024 1024" }
+                viewBox={ "0 0 16 16" }
                 fill={ props.fill.unwrap_or("currentColor") }
                 style={ style }
             >
-            <defs><style type="text/css"></style></defs><path d="M474 152m8 0l60 0q8 0 8 8l0 704q0 8-8 8l-60 0q-8 0-8-8l0-704q0-8 8-8Z" p-id="10298"></path><path d="M168 474m8 0l672 0q8 0 8 8l0 60q0 8-8 8l-672 0q-8 0-8-8l0-60q0-8 8-8Z" p-id="10299"></path>
+            <path d="M482 152h60q8 0 8 8v704q0 8-8 8h-60q-8 0-8-8V160q0-8 8-8Z"/>
+    <path d="M192 474h672q8 0 8 8v60q0 8-8 8H160q-8 0-8-8v-60q0-8 8-8Z"/>
             </svg>
         }
     }
