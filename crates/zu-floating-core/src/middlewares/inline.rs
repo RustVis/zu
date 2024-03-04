4f841863dd0103cdf3ef7f791320efd292cdbe54
Xu Shaohua <shaohua@biofan.org>
2024-03-04 21:58:30 +0800

    floating: Add inline data

--- crates/zu-floating-core/src/middlewares/inline.rs
+++ crates/zu-floating-core/src/middlewares/inline.rs
@@ -4,8 +4,22 @@
 
 //! Improves positioning for inline reference elements that span over multiple lines.
 
-use crate::middleware::{Middleware, MiddlewareReturn, MiddlewareState};
-use crate::types::{ClientRect, Padding, Rect};
+use crate::middleware::{Middleware, MiddlewareData, MiddlewareReturn, MiddlewareState};
+use crate::platform::ElementRects;
+use crate::types::{ClientRect, Padding, Rect, SideObject};
+
+#[derive(Debug, Default, Clone, PartialEq)]
+pub struct InlineMiddlewareData {
+    pub rects: ElementRects,
+}
+
+impl InlineMiddlewareData {
+    #[must_use]
+    #[inline]
+    pub fn from(data: &MiddlewareData) -> Option<&Self> {
+        data.get(Inline::NAME).map(|boxed| boxed.downcast_ref())?
+    }
+}
 
 #[derive(Debug, Clone)]
 pub struct Inline {
@@ -22,7 +36,7 @@ pub struct Inline {
     /// Represents the padding around a disjointed rect when choosing it.
     ///
     /// Default is 2
-    pub padding: Option<Padding>,
+    pub padding: Padding,
 }
 
 impl Default for Inline {
@@ -33,7 +47,7 @@ impl Default for Inline {
         Self {
             x: None,
             y: None,
-            padding: Some(Padding::Number(2.0)),
+            padding: Padding::Number(2.0),
         }
     }
 }
@@ -47,12 +61,62 @@ impl Middleware for Inline {
         Self::NAME
     }
 
-    fn run(&self, _state: &MiddlewareState) -> MiddlewareReturn {
-        todo!()
+    fn run(&self, state: &MiddlewareState) -> MiddlewareReturn {
+        let mut native_client_rects = state
+            .platform
+            .client_rects(&state.elements.reference)
+            .unwrap_or_default();
+        let client_rects = get_rect_by_line(&mut native_client_rects);
+        let fallback = get_bounding_rect(&native_client_rects).client_rect();
+        let padding_object: SideObject = self.padding.clone().into();
+
+        #[allow(clippy::no_effect_underscore_binding)]
+        let _get_bounding_client_rect = || -> ClientRect {
+            // There are two rects and they are disjointed.
+            if client_rects.len() == 2
+                && client_rects[0].side.left > client_rects[1].side.left
+                && self.x.is_some()
+                && self.y.is_some()
+            {
+                let x = self.x.unwrap_or_default();
+                let y = self.y.unwrap_or_default();
+                // Find the first rect in which the point is fully inside.
+                return client_rects
+                    .iter()
+                    .find(|rect| {
+                        x > rect.side.left - padding_object.left
+                            && x < rect.side.right + padding_object.right
+                            && y > rect.side.top - padding_object.top
+                            && y < rect.side.bottom + padding_object.bottom
+                    })
+                    .unwrap_or(&fallback)
+                    .clone();
+            }
+            // TODO(Shaohua): There are two or more connected rects.
+
+            fallback
+        };
+
+        let reset_rects = state.platform.element_rects(
+            &state.elements.reference,
+            &state.elements.floating,
+            state.strategy,
+        );
+
+        if state.rects.reference == reset_rects.reference {
+            MiddlewareReturn::default()
+        } else {
+            let inline_data = Box::new(InlineMiddlewareData { rects: reset_rects });
+            let data = MiddlewareData::with_value(Self::NAME, inline_data);
+            MiddlewareReturn {
+                data,
+                ..Default::default()
+            }
+        }
     }
 }
 
-pub fn get_bounding_rect(rects: &[&ClientRect]) -> Rect {
+pub fn get_bounding_rect(rects: &[ClientRect]) -> Rect {
     let min_x = rects
         .iter()
         .map(|obj| obj.rect.x)
@@ -84,7 +148,7 @@ pub fn get_bounding_rect(rects: &[&ClientRect]) -> Rect {
 pub fn get_rect_by_line(rects: &mut [ClientRect]) -> Vec<ClientRect> {
     rects.sort_by(|a, b| a.rect.y.total_cmp(&b.rect.y));
 
-    let mut groups: Vec<Vec<&ClientRect>> = Vec::new();
+    let mut groups: Vec<Vec<ClientRect>> = Vec::new();
     let mut prev_rect: Option<&ClientRect> = None;
     for rect in rects {
         let new_vec = if let Some(prev_rect) = prev_rect {
@@ -94,9 +158,9 @@ pub fn get_rect_by_line(rects: &mut [ClientRect]) -> Vec<ClientRect> {
         };
 
         if new_vec {
-            groups.push(vec![rect]);
+            groups.push(vec![rect.clone()]);
         } else if let Some(last_vec) = groups.last_mut() {
-            last_vec.push(rect);
+            last_vec.push(rect.clone());
         }
         prev_rect = Some(rect);
     }
