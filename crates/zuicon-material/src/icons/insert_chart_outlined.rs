0896ccb6e82d1f5001457643e64c7aafa6a171c9
Xu Shaohua <shaohua@biofan.org>
2024-11-06 20:59:21 +0800

    zuicon-material: Update copyright statement

--- crates/zuicon-material/src/icons/insert_chart_outlined.rs
+++ crates/zuicon-material/src/icons/insert_chart_outlined.rs
@@ -1,6 +1,6 @@
-// Copyright (c) 2023 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
-// Use of this source is governed by Lesser General Public License that can be
-// found in the LICENSE file.
+// Copyright (c) 2024 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
+// Use of this source is governed by Lesser General Public License
+// that can be found in the LICENSE file.
 
 // Auto generated, do not edit.
 
@@ -16,7 +16,7 @@ pub fn insert_chart_outlined(props: &Props) -> Html {
 
     html! {
         <SvgIcon ..new_props>
-            <path d="M0 0h24v24H0V0z" fill="none"/><path d="M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 16H5V5h14v14zM7 10h2v7H7zm4-3h2v10h-2zm4 6h2v4h-2z"/>
+            <path d="M0 0h24v24H0z" fill="none"/><path d="M9 17H7v-7h2v7zm4 0h-2V7h2v10zm4 0h-2v-4h2v4zm2.5 2.1h-15V5h15v14.1zm0-16.1h-15c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h15c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2z"/>
         </SvgIcon>
     }
 }
